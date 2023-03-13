d349ded27dd48a31a1bcbe6ffbd1642ce685c46b
dylancvdean <48204453+dylancvdean@users.noreply.github.com>
2023-03-13 17:18:58 -0600

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,27 +1,28 @@
-@echo off
+$hearing = Read-Host "Enter hearing video link"
+Invoke-WebRequest -UserAgent "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:118.0) Gecko/20100101 Firefox/118.0" -Uri $hearing -OutFile html
+Select-String "m3u8" html | Select-Object Line | Out-File html2
 
-set /p hearing="Enter hearing video link: "
+$url = (Select-String -Path html -Pattern 'https?://.*\.m3u8').Matches.Value | select-object -First 1
 
-powershell -Command "Invoke-WebRequest -UserAgent 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:118.0) Gecko/20100101 Firefox/118.0' -Uri '%hearing%' -OutFile html"
+Set-Content -Path url -Value $url
+$url = $url -replace '/playlist.m3u8$',''
 
-findstr "m3u8" html > html2
+$i = 1
+echo $url
+while ($true) {
+    $file_url = "$url/media_$i.ts"
+    echo $file_url
+    $response = Invoke-WebRequest -Uri $file_url -Method Head -UseBasicParsing -ErrorAction SilentlyContinue
 
-for /f "tokens=2 delims=:, " %%a in ('findstr /i /c:"url" html2') do set url=%%~a
+    if ($response.StatusCode -ne 200) {
+        break
+    }
 
-echo %url% > url
+    Invoke-WebRequest -Uri $file_url -OutFile "$i.ts"
 
-set /a i=1
+    $i++
+}
 
-:loop
-set file_url=%url%/media_%i%.ts
-powershell -Command "Invoke-WebRequest -Uri '%file_url%' -Method Head" >nul 2>&1
+& ffmpeg.exe -i "concat:$(Join-Path $PWD '*.ts')" -c copy output.mp4
 
-if %errorlevel% neq 0 goto done
-
-powershell -Command "Invoke-WebRequest -Uri '%file_url%' -OutFile %i%.ts"
-set /a i+=1
-goto loop
-
-:done
-copy /b *.ts output.mp4
-del *.ts
+Remove-Item -Path *.ts
